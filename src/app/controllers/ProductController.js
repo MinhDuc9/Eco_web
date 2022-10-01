cac4986eee21e640cc81833e71cd741396eb0c4d
MinhDuc9 <52440629+MinhDuc9@users.noreply.github.com>
2022-10-01 08:15:16 +0700

    Update ProductController.js

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -11,6 +11,20 @@ class ProductController {
             .catch(next);
     }
     
+    // [GET] /products/create
+    create(req, res, next) {
+        res.render('products/create');
+    }
+
+    // [POST] /products/store
+    store(req, res, next) {
+        const product = new Product(req.body);
+        product.save()
+            .then(() => res.redirect('/'))
+            .catch(error => {
+                
+            });
+    }
 }
 
 module.exports = new ProductController;

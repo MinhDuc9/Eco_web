265eeca551a6122abd8f364868769643fb590bba
MinhDuc9 <52440629+MinhDuc9@users.noreply.github.com>
2022-10-12 02:36:54 +0700

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const userController = require('../app/controllers/UserController')
 
 router.get('/stored/products', userController.storedProducts);
-
+router.get('/trash/products', userController.trashProducts);
 
 module.exports = router;

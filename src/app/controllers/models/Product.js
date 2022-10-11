6fc18b01621b76535664561f3b2e9af0b910724d
MinhDuc9 <52440629+MinhDuc9@users.noreply.github.com>
2022-10-12 02:34:12 +0700

    Update Product.js

--- src/app/controllers/models/Product.js
+++ src/app/controllers/models/Product.js
@@ -1,18 +1,27 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
+
 const Schema = mongoose.Schema;
 
+const Product = new Schema(
+    {
+        name: { type: String, required: true },
+        description: { type: String, },
+        image: { type: String, },
+        videoId: { type: String, required: true },
+        details: { type: String, },
+        slug: { type: String, slug: 'name', unique: true },
+    }, {
+        timestamps: true,
+    }
+);
+
+// Add plugins
 mongoose.plugin(slug);
-
-const Product = new Schema({
-    name: { type: String, required: true },
-    description: { type: String, },
-    image: { type: String, },
-    videoId: { type: String, required: true },
-    details: { type: String, },
-    slug: { type: String, slug: 'name', unique: true },
-}, {
-    timestamps: true,
+Product.plugin(mongooseDelete, { 
+    deletedAt : true, 
+    overrideMethods: 'all',
 });
-
+    
 module.exports = mongoose.model('Product', Product);

da48fd26b0ccab81d0c8215b01853bd98ae292e7
MinhDuc9 <52440629+MinhDuc9@users.noreply.github.com>
2022-10-12 02:36:12 +0700

    Update products.js

--- src/routes/products.js
+++ src/routes/products.js
@@ -9,7 +9,9 @@ router.get('/create', productController.create);
 router.post('/store', productController.store);
 router.get('/:id/edit', productController.edit);
 router.put('/:id', productController.update);
+router.patch('/:id/restore', productController.restore);
 router.delete('/:id', productController.delete);
+router.delete('/:id/force', productController.forceDelete);
 router.get('/:slug', productController.show);
 
 module.exports = router;

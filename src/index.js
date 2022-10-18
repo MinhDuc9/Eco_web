301c15f75e74c54965c533f2c6d65e31749a7ab5
MinhDuc9 <52440629+MinhDuc9@users.noreply.github.com>
2022-10-19 02:40:11 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const handlebars = require('express-handlebars').engine;
 const app = express();
 const port = 3000;
 
+const SortMiddleware = require('./app/controllers/middleware/SortMiddleware')
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -21,16 +23,45 @@ app.use(express.json());
 
 app.use(methodOverride('_method'));
 
+// Custom MiddleWare
+app.use(SortMiddleware);
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({
-  extname: '.hbs',
-  helpers: {
-    sum: (a, b) => a+b,
-  }
-}));
+app.engine(
+  'hbs', 
+  handlebars({
+    extname: '.hbs',
+    helpers: {
+      sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : 'default';
+
+        const icons = {
+          default: 'oi oi-elevator',
+          asc: 'oi oi-sort-ascending',
+          desc: 'oi oi-sort-descending',
+        };
+
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc',
+        }
+
+        const icon = icons[sortType];
+        const type = types[sortType];
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+          <span class="${icon}"></span>
+        </a>`;
+      }
+    }
+  })
+);
+
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
